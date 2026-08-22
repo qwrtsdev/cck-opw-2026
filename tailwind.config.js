fc6a2bd00a843a001fad6d02303140bf894c324e
qwrtsdev <potatoloadzch@gmail.com>
2026-08-23 03:36:48 +0700

    chore: prettier plugins and tailwind font configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        thai: ['Noto Sans Thai', 'sans-serif'],
+        pixel: ['2005_iannnnnAMD', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
+
 
